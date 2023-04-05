2d48064fd9a93490eb01931675e883879c73283e
tehi_kim <k.tehi21@gmail.com>
2023-04-05 05:06:50 +0000

    having, group by exercies

--- 2_queries_joins/2_join.sql
+++ 2_queries_joins/2_join.sql
@@ -10,3 +10,18 @@ ON student_id = students.id
 JOIN cohorts
 ON cohort_id = cohorts.id
 WHERE cohorts.name = 'FEB12';
+
+/* A currently enrolled student has a null end_date. */
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
+
+-- currently enrolled students who's total submissions are less than 100;
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL 
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
