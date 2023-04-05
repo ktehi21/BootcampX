da774821124fdc53f4cc9a009f0c4ea90d77f1d4
tehi_kim <k.tehi21@gmail.com>
2023-04-05 03:04:16 +0000

    BootcampX Assignments/ add assignments, assignment_submission TABLES and data

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -4,10 +4,3 @@ WHERE github IS NULL
 ORDER BY cohort_id;
 
 
-/* 
-  COMMAND TERMINAL에서 부르는 법
-  1. BootcampX 위치에서 psql 실행
-  2. \c bootcampx 실행해서 db 연결하기
-  3. \i 0_selects/1_students_without_github.sql 입력해서 해당 폴더의 파일에 명령어 수행하기
-
-*/
